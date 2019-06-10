08b973fa9b8fc096891138042d63ef6622fbf938
Martinez <michau.mathilde@gmail.com>
2019-06-10 22:51:21 +0200

    Search for a title and get a list of the 10 first founded books related

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,36 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld @booksFound="updateBooksFoundList"/>
+    <BooksFoundList :booksFoundList="booksFoundList"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import BooksFoundList from "./components/BooksFoundList.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    HelloWorld,
+    BooksFoundList
+  },
+  data() {
+    return { booksFoundList: [ ] };
+  },
+  methods: {
+    updateBooksFoundList(booklist) {
+      this.booksFoundList = booklist;
+    }
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
---
+++ src/components/BooksFoundList.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <ul v-if="booksFoundList">
+      <li v-for="bookItem in booksFoundList" v-bind:key="bookItem.id">
+        <span>{{ bookItem.volumeInfo.title }}</span>
+        <span v-if="bookItem.volumeInfo.authors"> - {{bookItem.volumeInfo.authors[0]}}</span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["booksFoundList"],
+  name: "BooksFoundList",
+  watch: {
+    booksFoundList: function(val) {
+      console.log(val);
+    }
+  }
+};
+</script>
+
