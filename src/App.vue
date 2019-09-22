6a861cbeca608d068f999de8ed1a4ccc658249ff
Mathilde <micamath@pc53.home>
2019-09-22 09:51:00 +0200

    add a navigation bar

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,31 @@
 <template>
   <v-app>
     <v-content>
-      <SearchBook @booksFound="updateBooksFoundList" />
-      <BooksFoundList :booksFoundList="booksFoundList" />
+      <Navigation />
+      <v-container>
+        <v-row>
+          <SearchBook @booksFound="updateBooksFoundList" />
+        </v-row>
+        <v-row>
+          <BooksFoundList :booksFoundList="booksFoundList" />
+        </v-row>
+      </v-container>
     </v-content>
   </v-app>
 </template>
 
+
 <script>
 import SearchBook from "./components/SearchBook";
 import BooksFoundList from "./components/BooksFoundList";
+import Navigation from "./components/Navigation";
 
 export default {
   name: "App",
   components: {
     SearchBook,
-    BooksFoundList
+    BooksFoundList,
+    Navigation
   },
   data: () => ({
     booksFoundList: []
---
+++ src/components/Navigation.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <!-- <v-navigation-drawer v-model="drawer" app>
+      <v-list dense>
+        <v-list-item @click>
+          <v-list-item-action>
+            <v-icon>mdi-home</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item @click>
+          <v-list-item-action>
+            <v-icon>mdi-contact-mail</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Contact</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer> -->
+
+    <v-app-bar app color="deep-orange" dark>
+      <!-- <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon> -->
+      <v-toolbar-title> BOOKSHELF </v-toolbar-title>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    source: String
+  },
+
+  data: () => ({
+    drawer: null
+  })
+};
+</script>
