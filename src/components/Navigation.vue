f796a4ba55fbc4cb4768257a66c45135b73642e3
micamath <michau.mathilde@gmail.com>
2020-04-18 15:12:41 +0200

    simplify Nav bar

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,29 +1,8 @@
 <template>
   <div>
-    <!-- <v-navigation-drawer v-model="drawer" app>
-      <v-list dense>
-        <v-list-item @click>
-          <v-list-item-action>
-            <v-icon>mdi-home</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>Home</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-        <v-list-item @click>
-          <v-list-item-action>
-            <v-icon>mdi-contact-mail</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>Contact</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer> -->
-
-    <v-app-bar app color="deep-orange" dark>
-      <!-- <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon> -->
-      <v-toolbar-title> BOOKSHELF </v-toolbar-title>
+    <v-app-bar app color="white" class="teal--text text--darken-4" flat>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>B O O K S H E L F</v-toolbar-title>
     </v-app-bar>
   </div>
 </template>
@@ -39,3 +18,7 @@ export default {
   })
 };
 </script>
+
+<style  scoped>
+
+</style>
