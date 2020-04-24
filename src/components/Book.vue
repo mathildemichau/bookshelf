8154eab49f8eb4e09dd8911373fadb8ea73bdcd0
micamath <michau.mathilde@gmail.com>
2020-04-24 13:36:49 +0200

    force author and title to be on differenrs lines

--- src/components/Book.vue
+++ src/components/Book.vue
@@ -4,12 +4,11 @@
       <v-img :src="cover" height="120"></v-img>
     </v-container>
     <v-container height="100%" fluid class="ma-0 pa-0">
-      <v-card-title min-height="100" class="ma-0 pa-3 text-truncate" height="100">
-        <span class="text-truncate d-inline-block">{{title}}</span>
-        <br />
-        <span class="grey--text body-2 d-inline-block text-truncate">{{author}}</span>
+      <v-card-title min-height="100" class="ma-0 px-4 pb-1 text-truncate" height="100">
+        <div class="text-truncate">{{title}}</div>
       </v-card-title>
       <v-card-text>
+        <div class="mb-2 grey--text body-2 text-truncate">{{author}}</div>
         <div class="text-left">
           <v-rating
             color="teal"
