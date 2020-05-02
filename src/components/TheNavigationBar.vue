e9414b37081486d67ef6511c9e91184cd4985db9
Ibi <ibi.andrianoasy+git@gmail.com>
2020-05-02 15:25:40 +0200

    added bookshelf logo to app bar

--- src/components/TheNavigationBar.vue
+++ src/components/TheNavigationBar.vue
@@ -1,5 +1,6 @@
 <template>
   <v-app-bar app color="indigo lighten-3" flat dark>
+    <v-icon class="mr-2">mdi-bookshelf</v-icon>
     <v-toolbar-title>B O O K S H E L F</v-toolbar-title>
   </v-app-bar>
 </template>
