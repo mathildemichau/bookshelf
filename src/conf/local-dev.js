a4911ff0cac5b1ca80819bff5b55d47982022152
Ibi <ibi.andrianoasy+git@gmail.com>
2020-05-06 00:56:38 +0200

    eased mock database structure to provide information for a single user

--- src/conf/local-dev.js
+++ src/conf/local-dev.js
@@ -1,47 +1,41 @@
 const mockDb = [
   {
-    isbn: '0060956666',
-    title: 'Democracy in America',
-    author: 'Alexis de Tocqueville',
-    cover: 'https://covers.openlibrary.org/b/id/42029-L.jpg',
-    readings: [
-      {
-        dateRead: '',
-        rating: '',
-        comment: 'Cool book'
-      }
-    ]
+    isbn: "0060956666",
+    title: "Democracy in America",
+    author: "Alexis de Tocqueville",
+    cover: "https://covers.openlibrary.org/b/id/42029-L.jpg",
+    reading: {
+      dateRead: "",
+      rating: "",
+      comment: "Cool book",
+    },
   },
   {
-    isbn: '0498025411',
-    title: 'Relax & win',
-    author: 'Bud Winter',
-    cover: 'https://covers.openlibrary.org/b/id/9368754-L.jpg',
-    readings: [
-      {
-        dateRead: '',
-        rating: '',
-        comment: 'Bad book'
-      }
-    ]
+    isbn: "0498025411",
+    title: "Relax & win",
+    author: "Bud Winter",
+    cover: "https://covers.openlibrary.org/b/id/9368754-L.jpg",
+    reading: {
+      dateRead: "",
+      rating: "",
+      comment: "Bad book",
+    },
   },
   {
-    isbn: '9780613003124',
-    title: 'Wild, Wild Hair',
-    author: 'Nikki Grimes',
-    cover: 'https://covers.openlibrary.org/b/id/1316204-L.jpg',
-    readings: [
-      {
-        dateRead: '',
-        rating: '',
-        comment: 'Bad book'
-      }
-    ]
+    isbn: "9780613003124",
+    title: "Wild, Wild Hair",
+    author: "Nikki Grimes",
+    cover: "https://covers.openlibrary.org/b/id/1316204-L.jpg",
+    reading: {
+      dateRead: "",
+      rating: "",
+      comment: "Bad book",
+    },
   },
-]
+];
 
 const localDev = {
-  db: mockDb
-}
+  db: mockDb,
+};
 
-export default localDev
+export default localDev;
